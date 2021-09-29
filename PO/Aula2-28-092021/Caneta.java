6db48955c51e3d94a2ae2d9e115bd4dcbbb97c4c
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2021-09-29 14:32:06 +0100

    second class

--- PO/Aula2-28-092021/Caneta.java
+++ PO/Aula2-28-092021/Caneta.java
@@ -1,29 +1,37 @@
 public class Caneta {
 
-	int quantidade; 	// valor por omissao para int, double, float... é 0
-	String marca;		// valor por omissao é null
-	boolean tapada; 	// valor por omissao = false
-	static int numeroCanetas;	//atributo estatico {static} -> partilhado por todas as instancias da classe (acesso ao mesmo sitio da memoria)
+	private int _quantidade; 	// valor por omissao para int, double, float... é 0
+	private String _marca;		// valor por omissao é null
+	private boolean _tapada; 	// valor por omissao = false
+
+	private static int _numeroCanetas = 56;	//atributo estatico {static} -> partilhado por todas as instancias da classe (acesso ao mesmo sitio da memoria)
 
 	//todos os metodos declarados dentro da classe têm acesso aos seus atributos
 
 	public void tapar() {
-		tapada = true;
+		_tapada = true;
 	}
 
 	public void destapar() {
-		tapada = false;
+		_tapada = false;
 	}
 
 	public String obtemMarca() {
-		return marca;
+		return _marca;
+	}
+
+	public boolean encher(int q) {
+		if (q <= 0 ) 
+			return false;
+		_quantidade += q;
+		return true;
 	}
 
 	public boolean escreve(String str) {
-		if (str.length() > quantidade) 
+		if (str.length() > _quantidade) 
 			return false;
 		
-		quantidade -= str.length();
+		_quantidade -= str.length();
 		// System.out.printf("%s\n", str); -> possivel para matar saudades de C <3
 		System.out.println(str);
 	}
@@ -33,10 +41,13 @@ public class Caneta {
 	// cada atributo é PRIVADO para cada objeto
 
 	public Caneta(int q, String m, boolean b) {		//metodo de inicializacao dos atributos do objeto, construtor (obriga a ser usado)
+		this(q, m , true); 							//permite fazer poupança de codigo, mesma função que as linhas abaixo {37, 42}
+		/*
 		quantidade = q;
 		marca = m;
 		tapada = b;
 		numeroCanetas++;
+		*/
 	}
 
 	public static void main(String[] args) {	//String[] <=> char **
@@ -44,7 +55,7 @@ public class Caneta {
 		Caneta c2 = new Caneta(80, "Marca 1", true);
 		Caneta c3 = new Caneta(70, "Marca 50", true)
 
-		c2.quantidade = 80;
+		c2._quantidade = 80;
 		
 		//c1.init(70, "Marca 2", false);	//inicializacao correta, no caso da criacao do metodo init()
 
