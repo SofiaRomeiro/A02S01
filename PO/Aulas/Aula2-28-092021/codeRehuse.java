dac1f13cb718ca7422bb52476c9c8fef3425d46a
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2021-10-01 17:30:04 +0100

    2nd class

---
+++ PO/Aulas/Aula2-28-092021/codeRehuse.java
@@ -0,0 +1,16 @@
+/*
+Reutilização de código
+
+	- Composição
+	- Herança
+
+*/
+
+public class codeRehuse {
+
+	public void move(int dx, int dy) {
+		_origin.move(dx, dy);
+		_end.move(dx, dy);
+	}
+
+}
